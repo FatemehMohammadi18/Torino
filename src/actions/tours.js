59382576549b0d245cced94ed667b62e7614254c
Fatemeh A-Mohammadi <129193825+FatemehMohammadi18@users.noreply.github.com>
2025-08-09 13:47:51 +0330

    bug-fix-filter-tour-data

--- src/actions/tours.js
+++ src/actions/tours.js
@@ -1,30 +1,16 @@
 "use server";
-export async function getData(filters = {}) {
-  if (typeof filters.then === "function") {
-    filters = await filters;
-  }
-
-  const plainFilters = filters.entries
-    ? Object.fromEntries([...filters.entries()])
-    : filters;
-
-  const safeFilters = Object.fromEntries(
-    Object.entries(plainFilters).map(([key, value]) => [
-      String(key),
-      String(value),
-    ])
-  );
-
-  const params = new URLSearchParams(safeFilters).toString();
-
-  const res = await fetch(
-    `http://localhost:6500/tour${params ? "?" + params : ""}`,
-    {
-      cache: "no-store",
-    }
-  );
-
-  if (!res.ok) return;
+export default async function getData(filters = {}) {
+  console.log(filters);
+  const query = new URLSearchParams(filters).toString();
+  console.log(query);
+  const url = query
+    ? `http://localhost:6500/tour?${query}`
+    : `http://localhost:6500/tour`;
+  console.log(url);
+  const res = await fetch(url, {
+    cache: "no-store",
+  });
+  if (!res.ok) return null;
 
   const data = await res.json();
   return data;
