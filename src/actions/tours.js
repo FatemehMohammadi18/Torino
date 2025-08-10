5f80079eefdca43d406d7426096a3c31c281704b
Fatemeh A-Mohammadi <129193825+FatemehMohammadi18@users.noreply.github.com>
2025-08-10 12:52:11 +0330

    bugfix-filter-tours-in-homepage

--- src/actions/tours.js
+++ src/actions/tours.js
@@ -1,12 +1,8 @@
 "use server";
-export default async function getData(filters = {}) {
-  console.log(filters);
-  const query = new URLSearchParams(filters).toString();
-  console.log(query);
+export default async function getData(query = "") {
   const url = query
     ? `http://localhost:6500/tour?${query}`
     : `http://localhost:6500/tour`;
-  console.log(url);
   const res = await fetch(url, {
     cache: "no-store",
   });
--- src/components/modules/home/SearchTour.js
+++ src/components/modules/home/SearchTour.js
@@ -1,16 +1,19 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import PersianDatePicker from "@/components/public/PersianDatePicker";
-import { getData } from "actions/tours";
 
-export default function SearchTour() {
+import getData from "@/actions/tours";
+
+import PersianDatePicker from "@/components/public/PersianDatePicker";
+
+function SearchTour() {
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
-  const [dateRange, setDateRange] = useState([null, null]);
   const [origins, setOrigins] = useState([]);
   const [destinations, setDestinations] = useState([]);
+  const [dateRange, setDateRange] = useState([null, null]);
+
   const router = useRouter();
 
   useEffect(() => {
@@ -34,27 +37,26 @@ export default function SearchTour() {
     fetchTours();
   }, []);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
     const [startDate, endDate] = dateRange;
 
-    if (!origin || !destination || !startDate || !endDate) {
-      alert("لطفاً همه فیلدها را پر کنید");
-      return;
-    }
+    const startDateObj = startDate.toDate();
+    startDateObj.setUTCHours(0, 0, 0, 0);
+    const startDateISO = startDateObj.toISOString();
 
-    const startISO = convertDateObjectToISO(startDate);
-    const endISO = convertDateObjectToISO(endDate);
+    const endDateObj = endDate.toDate();
+    endDateObj.setUTCHours(0, 0, 0, 0);
+    const endDateISO = endDateObj.toISOString();
 
-    const query = new URLSearchParams({
-      destinationId: destination,
-      originId: origin,
-      startDate: startISO,
-      endDate: endISO,
-    }).toString();
+    const filters = {};
+    if (destination) filters.destinationId = destination;
+    if (origin) filters.originId = origin;
+    filters.startDate = startDateISO;
+    filters.endDate = endDateISO;
 
-    router.push(`/tour?${query}`);
+    const query = new URLSearchParams(filters).toString();
+    router.push(`/?${query}`);
   };
 
   return (
@@ -63,62 +65,58 @@ export default function SearchTour() {
         <span className="text-primary ml-2 mb-3">تورینو</span>
         برگزارکننده بهترین تورهای داخلی و خارجی
       </h1>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-wrap justify-center lg:justify-between mt-4 gap-4 lg:gap-0 lg:border-[1px] lg:p-3 lg:rounded-[20px]"
-      >
-        <select
-          name="origin"
-          value={origin}
-          onChange={(e) => setOrigin(e.target.value)}
-          className="origin w-5/12 h-[47px] appearance-none border rounded px-8 py-1 text-center lg:border-0 lg:w-[23%] lg:text-start lg:px-2"
+      <div className="relative">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-wrap justify-center lg:justify-between mt-4 gap-4 lg:gap-0 lg:border-[1px] lg:p-3 lg:rounded-[20px]"
         >
-          <option value="" className="hidden">
-            مبدا
-          </option>
-          {origins.map(([id, name]) => (
-            <option key={id} value={id}>
-              {name}
+          <select
+            name="origin"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+            className="origin w-5/12 h-[47px] appearance-none border rounded px-8 py-1 text-center lg:border-0 lg:w-[23%] lg:text-start lg:px-2"
+          >
+            <option value="" className="hidden">
+              مبدا
             </option>
-          ))}
-        </select>
-        <hr className="hidden lg:block w-0.5 min-h-[50px] bg-black/60" />
-        <select
-          name="destination"
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-          className="destination w-5/12 h-[47px] appearance-none border rounded px-8 py-1 text-center lg:border-0 lg:w-[23%] lg:text-start lg:px-2"
-        >
-          <option value="" className="hidden">
-            مقصد
-          </option>
-          {destinations.map(([id, name]) => (
-            <option key={id} value={id}>
-              {name}
+            {origins.map(([id, name]) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </select>
+          <hr className="hidden lg:block w-0.5 min-h-[50px] bg-black/60" />
+          <select
+            name="destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            className="destination w-5/12 h-[47px] appearance-none border rounded px-8 py-1 text-center lg:border-0 lg:w-[23%] lg:text-start lg:px-2"
+          >
+            <option value="" className="hidden">
+              مقصد
             </option>
-          ))}
-        </select>
-        <hr className="hidden lg:block w-0.5 min-h-[50px] bg-black/60" />
+            {destinations.map(([id, name]) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </select>
+          <hr className="hidden lg:block w-0.5 min-h-[50px] bg-black/60" />
 
-        <PersianDatePicker dateRange={dateRange} setDateRange={setDateRange} />
-
-        <button
-          type="submit"
-          className="min-w-full bg-primary text-white px-4 py-2 rounded-2xl text-xl font-normal lg:min-w-1/4"
-        >
-          جستجو
-        </button>
-      </form>
+          <PersianDatePicker
+            dateRange={dateRange}
+            setDateRange={setDateRange}
+          />
+          <button
+            type="submit"
+            className="min-w-full bg-primary text-white px-4 py-2 rounded-2xl text-xl font-normal lg:min-w-1/4"
+          >
+            جستجو
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
 
-import gregorian from "react-date-object/calendars/gregorian";
-
-function convertDateObjectToISO(dateObj) {
-  if (!dateObj || typeof dateObj.convert !== "function") return null;
-
-  const gregorianDate = dateObj.convert(gregorian);
-  const date = gregorianDate.toDate().toISOString();
-  return date;
-}
+export default SearchTour;
