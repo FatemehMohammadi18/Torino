69d5d50a59043b1f83dce53370aa104eae709be9
Fatemeh A-Mohammadi <129193825+FatemehMohammadi18@users.noreply.github.com>
2025-08-23 23:01:33 +0330

    Add API rewrites and remote image patterns

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,22 @@ const nextConfig = {
         port: "6500",
         pathname: "/static/images/**",
       },
+      {
+        protocol: "https",
+        hostname: "constant-kiersten-my-personal-frontend-project-322b1f0a.koyeb.app",
+        pathname: "/static/images/**",
+      },
     ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://constant-kiersten-my-personal-frontend-project-322b1f0a.koyeb.app/:path*",
+      },
+    ];
   },
 };
 
